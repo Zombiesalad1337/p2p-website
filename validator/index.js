8b8e0e69f785334e8e4c07df00777370d301a26f
Rohit Choudhary <rohit01031999@gmail.com>
2022-12-05 11:21:55 +0530

    fixed validation bug

--- validator/index.js
+++ validator/index.js
@@ -1,12 +1,12 @@
 exports.userSignupValidator = (req, res, next) => {
   req.check('name', 'Name is required').notEmpty();
   req
-    .check('email', 'Email must be between 3 to 32 characters')
+    .check('email', 'Email must be between 3 to 128 characters')
     .matches(/.+\@.+\..+/)
     .withMessage('Email must contain @')
     .isLength({
       min: 4,
-      max: 32,
+      max: 128,
     });
   req.check('password', 'Password is required').notEmpty();
   req
